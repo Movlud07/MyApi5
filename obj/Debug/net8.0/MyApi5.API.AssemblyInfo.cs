91b50bf00e24a1b08842c65a3c62712ef7367180
Movlud07 <movlud.mirzezade.07@gmail.com>
2025-08-05 17:40:50 +0400

    addednewtextfilenameis:myName

--- obj/Debug/net8.0/MyApi5.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi5.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi5.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e44e77f7ab5c3b4fe2a335c69e21d4c92be4190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3afabb7bd5bc3687621e0fa1ec55a41ddf7c817")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi5.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi5.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
